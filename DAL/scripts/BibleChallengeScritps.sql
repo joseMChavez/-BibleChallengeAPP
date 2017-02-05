20328a03ad7184c9ab5109628dd436013cff6a81
Jose Maximino Chavez <josemc.ortiz@gmail.com>
2017-02-04 23:56:13 -0400

    Asiendo ajustes, conectando la base de datos y creando algunas tablas

---
+++ DAL/scripts/BibleChallengeScritps.sql
@@ -0,0 +1,17 @@
+﻿create table Participantes(
+     ParticipanteId int identity(1,1) primary key,
+	 EquipoId int References Equipos(EquipoId),
+	 Nombres varchar(120),
+	 Apodo varchar(40),
+	 Genero int,
+	 Fecha Date,
+	 FechaNacimiento DateTime
+)
+
+go
+
+Create table Equipos(
+
+EquipoId int identity(1,1) primary key,
+Descripcion varchar(70)
+)
